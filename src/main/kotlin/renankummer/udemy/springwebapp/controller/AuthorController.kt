0c2032ad04963644bd7a4edd4e8d335ea8697e14
Renan Kummer <dev.renankummer@outlook.com>
2020-09-26 15:17:39 -0300

    :recycle: Replace model.addAttribute() with Kotlin's [] syntax

--- src/main/kotlin/renankummer/udemy/springwebapp/controller/AuthorController.kt
+++ src/main/kotlin/renankummer/udemy/springwebapp/controller/AuthorController.kt
@@ -2,6 +2,7 @@ package renankummer.udemy.springwebapp.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import renankummer.udemy.springwebapp.repository.AuthorRepository
 
@@ -10,7 +11,7 @@ class AuthorController(val authorRepository: AuthorRepository) {
 
     @GetMapping("/authors")
     fun getAuthors(model: Model): String {
-        model.addAttribute("authors", authorRepository.findAll())
+        model["authors"] = authorRepository.findAll()
         return "authors/list"
     }
 }
--- src/main/kotlin/renankummer/udemy/springwebapp/controller/BookController.kt
+++ src/main/kotlin/renankummer/udemy/springwebapp/controller/BookController.kt
@@ -2,6 +2,7 @@ package renankummer.udemy.springwebapp.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import renankummer.udemy.springwebapp.repository.BookRepository
 
@@ -10,7 +11,7 @@ class BookController(val bookRepository: BookRepository) {
 
     @GetMapping("/books")
     fun getBooks(model: Model): String {
-        model.addAttribute("books", bookRepository.findAll())
+        model["books"] = bookRepository.findAll()
         return "books/list"
     }
 }
